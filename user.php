efe96fca7918a6d9aeab5ec15b6488aa5a2783b6
elfat-py <elfatmemaj@gmail.com>
2024-05-28 17:25:17 +0200

    did styling

--- user.php
+++ user.php
@@ -2,7 +2,6 @@
 require_once 'includes/header.php';
 ?>
 
-
 <?php
 function get_user_id($conn, $username)
 {
@@ -28,7 +27,8 @@ function get_user_id($conn, $username)
     }
 }
 
-function get_user_post_ids($conn, $username){
+function get_user_post_ids($conn, $username)
+{
     $sql = "SELECT p.post_ID FROM post p JOIN user u ON p.author_ID = u.user_ID WHERE u.username = ?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -59,6 +59,7 @@ function get_user_post_ids($conn, $username){
         return $post_ids;
     }
 }
+
 ?>
 
 <?php
@@ -69,59 +70,94 @@ if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
     $fullName = $_SESSION['fullName'];
     $_SESSION['user_ID'] = get_user_id($conn, $username);
     $_SESSION['post_of_user'] = get_user_post_ids($conn, $username);
+    ?>
+    <!-- Meaby the user can select here his blog posts and overall blog posts -->
+    <div class="container text-center">
+        <div class="row align-items-center">
+            <div class="col">
+                all post
+            </div>
+            <div class="col">
+                admin posts
+            </div>
+            <div class="col">
+                my posts
+            </div>
+        </div>
+    </div>
 
-    echo '<ul class="list-group">';
-    echo '<li class="list-group-item active" aria-current="true">Displaying some data for the user</li>';
-    echo '<li class="list-group-item">' . 'Your full name is: ' . $fullName . '</li>';
-    echo '<li class="list-group-item">' . 'Your username is: ' . $username . '</li>';
-    echo '<li class="list-group-item">' . 'Your age is: ' . $age . '</li>';
-    echo '<li class="list-group-item">' . 'Your email is: ' . $email . '</li>';
-    echo '<li class="list-group-item">' . 'Your user ID is: ' . get_user_id($conn, $username) . '</li>';
-    echo '</ul>';
+    <!--The User information section of the user page -->
+    <div class="row p-0">
+        <div class="col-4 p-0">
+            <ul class="list-group">
+                <li class="list-group-item bg-info text-dark" aria-current="true">User</li>
+                <li class="list-group-item">
+                    <label class="text-body-secondary text-decoration-underline">Full name : </label>
+                    <label class="fs-6 text-capitalize fw-medium"><?php echo $fullName ?></label>
+                </li>
+                <li class="list-group-item">
+                    <label class="text-body-secondary text-decoration-underline">Username : </label>
+                    <label class="fs-6 text-capitalize fw-medium"><?php echo $username ?></label>
+                </li>
+                <li class="list-group-item">
+                    <label class="text-body-secondary text-decoration-underline">Age : </label>
+                    <label class="fs-6 text-capitalize fw-medium"><?php echo $age ?></label>
+                </li>
+                <li class="list-group-item">
+                    <label class="text-body-secondary text-decoration-underline">Email : </label>
+                    <label class="fs-6 text-capitalize fw-medium"><?php echo $email ?></label>
+                </li>
+                <li class="list-group-item">
+                    <label class="text-body-secondary text-decoration-underline">User ID : </label>
+                    <label class="fs-6 text-capitalize fw-medium"><?php echo get_user_id($conn, $username) ?></label>
+                <li class="list-group-item">
+                    <a href="includes/add-new-post.php">
+                        <button type="button" class="btn btn-primary d-flex">Add new post</button>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
+        <!--The blog section of the user -->
+        <div class="col-8 p-0">
+            <ul class="list-group">
+                <li class="list-group-item active" aria-current="true">Blog</li>
+                <div class="list-group-item">
+                    <?php
+                    $user_ID = get_user_id($conn, $username);
+                    $sql = "SELECT title, created, body, post_ID FROM post WHERE author_ID = $user_ID";
+                    $result = mysqli_query($conn, $sql);
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <div class="row text-end">
+                                    <div class="col">
+                                        <a href="includes/delete-post.php?post_ID=<?php echo $row['post_ID'] ?>"
+                                           class="btn btn-danger btn-sm">Delete</a>
+                                        <a href="includes/edit-post.php?post_ID=<?php echo $row['post_ID'] ?>"
+                                           class="btn btn-warning btn-sm">Edit</a>
+                                    </div>
+                                </div>
+                                <h5 class="card-title text-center m-0 fw-light"><?php echo $row['title'] ?></h5>
+                                <p class="card-title text-end m-0 fst-italic"><?php echo $row['created'] ?></p>
+                                <hr style="width: 50%; margin: auto">
+                                <p class="card-text text-center p-4 fw-lighter"><?php echo $row['body'] ?></p>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 
-
+    <?php
 
 } else {
     echo '<h1>You haven\'t logged in there is no content to show</h1>';
-}
-?>
-
-    <a href="includes/add-new-post.php">
-        <button type="button" class="btn btn-primary d-flex justify-content-end">Add new post</button>
-    </a>
-<?php
-
-$user_ID = get_user_id($conn, $username);
-$sql = "SELECT title, created, body, post_ID FROM post WHERE author_ID = $user_ID"; // Replace with your actual query
-$result = mysqli_query($conn, $sql);
-while ($row = mysqli_fetch_assoc($result)) {
-    ?>
-
-    <br>
-
-    <section class="posts-container">
-        <article class="post">
-            <header class="main-header">
-                <div class="header-container">
-                    <h1 class="title-post"><?php echo $row['title'] ?></h1>
-                    <br>
-                    <div class="about-post">by <?php echo $username ?> on <?php echo $row['created'] ?></div>
-                    <br>
-                </div>
-            </header>
-            <p class="body-post"><?php echo $row['body'] ?></p>
-        </article>
-
-        <div class="col-1 border border-light-subtle bg-dark-subtle text-dark-emphasis">
-            <a href="includes/delete-post.php?post_ID=<?php echo $row['post_ID'] ?>" class="link-dark">Delete<i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
-        </div>
-        <div class="col-1 border border-light-subtle bg-dark-subtle text-dark-emphasis">
-            <a href="includes/edit-post.php?post_ID=<?php echo $row['post_ID'] ?>" class="link-dark">Edit<i class="fa-solid fa-pen-to-square fs-5 me-3"></i></a>
-        </div>
-    </section>
-    <hr>
-    <?php
 }
 ?>
 
